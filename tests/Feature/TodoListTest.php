63396d9fb32fff8821787308283e0e6a5a11c185
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-02-10 11:05:04 -0300

    First Test API passed

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_store_todo_list()
+    {
+        // preparation / prepare
+
+        // action / performe
+
+        // assertion / predict
+
+        $this->withoutExceptionHandling();
+        
+        $response = $this->getJson(route('todo-list.store'));
+
+        // $response = $this->get('/');
+        // dd($response->json());
+        
+        $this->assertEquals(1,count($response->json()));
+    }
+}
