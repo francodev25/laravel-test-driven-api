12ae13d3c9e8f6272a1fe2896022d84c7241b5f4
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-03-08 22:14:46 -0300

    Complete last commit | Register User
    All the data from the last commit was not added.

---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_a_user_can_register()
+    {
+        $this->withExceptionHandling();
+        
+        $this->postJson(route('user.register'),
+            ['name' => 'Franco', 
+             'email' => 'franco@gmail.com',
+             'password' => 'secret123',
+             'password_confirmation' => 'secret123'
+            ])->assertCreated();
+        
+        $this->assertDatabaseHas('users', ['name'=>'Franco']);
+    }
+}
