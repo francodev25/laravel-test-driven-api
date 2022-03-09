f1a84c7657187c6b01a4780d200967513baf5ddf
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-03-08 22:13:16 -0300

    Register a User

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Http\Request;
@@ -19,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('todo-list',TodoListController::class);
 
 Route::apiResource('todo-list.task',TaskController::class)->except('show')->shallow();
+
+
+Route::post('/register',RegisterController::class)->name('user.register');
