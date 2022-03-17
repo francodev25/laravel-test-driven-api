d1fc437f77f85dc7f249f37fb23dcbb4a92e1d79
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-03-17 18:59:44 -0300

    Seeder DB and Documentation API upgrade

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function(){
     
-    Route::apiResource('todo-list',TodoListController::class);
     
     Route::apiResource('todo-list.task',TaskController::class)->except('show')->shallow();
 });
 
+Route::apiResource('todo-list',TodoListController::class);
 
 
 Route::post('/register',RegisterController::class)->name('user.register');
